cf69579d50bf67d38df3778d60d63a3e5564bcb6
Daniel Garrido <86997452+Daniel-Garrido@users.noreply.github.com>
2024-08-02 16:25:57 -0600

    cambios en los archivos

--- js/js.js
+++ js/js.js
@@ -1,35 +1,52 @@
-function loadHeader() {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'pages/header.html', true);
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      document.getElementById('header-container').innerHTML = xhr.responseText;
-      // Cargar y ejecutar header.js después de insertar el header
-      const script = document.createElement('script');
-      script.src = 'pages/header.js';
-      script.onload = function() {
-        loadContent('pages/home_page'); // Cargar el contenido inicial
-      };
-      document.body.appendChild(script);
-    }
-  };
-  xhr.send();
-}
+/**variables generales */
+const info_parrafo = document.querySelectorAll(".project-description"); //selecciona todos los parrafos con esta clase
+const btn_menu = document.querySelectorAll(".dropdown-menu .dropdown-item"); //selecciona todos los enlaces dentro del submenu
 
-function loadContent(page) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', page + '.html', true);
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      document.getElementById('content').innerHTML = xhr.responseText;
-    }
-  };
-  xhr.send();
-}
+//Evento para cerrar el menu de navegacion
+document.addEventListener("DOMContentLoaded", function () {
+  btn_menu.forEach((link) => {
+    link.addEventListener("click", function () {
+      // Selecciona el menú de navegación colapsado
+      const navbarCollapse = document.querySelector("#navbarSupportedContent");
 
-document.addEventListener('DOMContentLoaded', function() {
-  loadHeader(); // Cargar el header dinámicamente
-  // Cargar el footer de manera estática
-  const footer = document.getElementById('footer-container');
-  footer.innerHTML = '<iframe src="pages/footer.html" frameborder="0" width="100%" height="100"></iframe>';
+      if (navbarCollapse.classList.contains("show")) {
+        // crear una instancia de Bootstrap Collapse
+        const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+          toggle: false,
+        });
+        bsCollapse.hide(); // Ocultar el menú
+      }
+    });
+  });
+});
+
+/*evento para guardar la informacion de los parrafos */
+document.querySelector(".toggle-button").addEventListener("click", function () {
+  const icon = this;
+  info_parrafo.forEach(function (parrafos) {
+    if (parrafos.classList.contains("hidden")) {
+      parrafos.style.height = parrafos.scrollHeight + "px";
+      parrafos.style.opacity = 1;
+      parrafos.classList.remove("hidden");
+      //remover el icono incial
+      icon.classList.remove("fa-chevron-right");
+      icon.classList.add("fa-chevron-down");
+
+      /**tiempo de ejecucion de los estilos */
+      setTimeout(function () {
+        parrafos.style.height = "auto";
+      }, 20); //tiempo al cargar de los parrafos
+    } else {
+      parrafos.style.height = parrafos.scrollHeight + "px";
+      setTimeout(function () {
+        parrafos.style.height = "0";
+        parrafos.style.opacity = 0;
+        parrafos.classList.add("hidden");
+
+        //revovemos el icono y lo volvemos a la posicion inicial
+        icon.classList.remove("fa-chevron-down");
+        icon.classList.add("fa-chevron-right");
+      }, 20); // tiempo de cerrar de los parrafos
+    }
+  });
 });
